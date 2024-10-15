b5f934551ca5c6da34bb9e8cf284467cb830af9c
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-15 12:55:30 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-  const hostname = req.headers.get("host") || "";
+// export function middleware(req: NextRequest) {
+//   const url = req.nextUrl.clone();
+//   const hostname = req.headers.get("host") || "";
 
-  if (hostname.startsWith("blogs")) {
-    url.pathname = `/blogs${url.pathname}`;
-    return NextResponse.rewrite(url);
-  }
+//   if (hostname.startsWith("blogs")) {
+//     url.pathname = `/blogs${url.pathname}`;
+//     return NextResponse.rewrite(url);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
+
+
