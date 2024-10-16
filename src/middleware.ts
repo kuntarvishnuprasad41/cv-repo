c7c3c2a6d5016909ae082814d35c38b991a40c40
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-16 12:37:03 +0530

    path

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,12 @@ export function middleware(req: NextRequest) {
 
   const subdomain = hostname.split(".")[0];
   const hostLen = hostname.split(".").length;
-  console.log("hn:", hostLen);
+ 
 
   if (subdomain !== "www" && hostLen > 2) {
     url.pathname = `/${subdomain}${url.pathname}`;
+    console.log("patj", url.pathname);
+
     return NextResponse.rewrite(url);
   }
 
