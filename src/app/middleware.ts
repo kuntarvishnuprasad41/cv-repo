918ccca55be2ddd4f24673409f3f593ba9c07605
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-16 10:51:19 +0530

    cv

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,9 +4,16 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const hostname = req.headers.get("host") || "";
 
-  if (hostname.startsWith("blogs")) {
+  console.log("hn:", hostname, "\n url:", url);
+
+  if (hostname.startsWith("blog")) {
     // Rewrite to the blogs section
-    url.pathname = `/blogs${url.pathname}`;
+    url.pathname = `/blog${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+  if (hostname.startsWith("cv")) {
+    // Rewrite to the blogs section
+    url.pathname = `/cv${url.pathname}`;
     return NextResponse.rewrite(url);
   }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-
   const url = req.nextUrl.clone();
   const hostname = req.headers.get("host") || "";
 
   console.log("hn:", hostname, "\n url:", url);
-  
 
   if (hostname.startsWith("blog")) {
     // Rewrite to the blogs section
     url.pathname = `/blog${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+  if (hostname.startsWith("cv")) {
+    // Rewrite to the blogs section
+    url.pathname = `/cv${url.pathname}`;
     return NextResponse.rewrite(url);
   }
 
