8f0f7cd8f40975942be31aaf65ef67909304d4ec
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-16 10:21:48 +0530

    Blog host

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const hostname = req.headers.get("host") || "";
 
-  if (hostname.startsWith("blogs")) {
+  if (hostname.startsWith("blog")) {
     // Rewrite to the blogs section
-    url.pathname = `/blogs${url.pathname}`;
+    url.pathname = `/blog${url.pathname}`;
     return NextResponse.rewrite(url);
   }
 
