44c1f52a8af94f43e1591db3e1d157ae33855a3f
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-16 12:30:33 +0530

    conditional subdomain redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,12 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const hostname = req.headers.get("host") || "";
 
-  console.log("hn:", hostname, "\n url:", url);
-
+  
   const subdomain = hostname.split(".")[0];
+  const hostLen = hostname.split(".").length < 2;
+  console.log("hn:", !hostLen);
 
-  if (subdomain !== "www") {
+  if (subdomain !== "www" && !hostLen) {
     url.pathname = `/${subdomain}${url.pathname}`;
     return NextResponse.rewrite(url);
   }
