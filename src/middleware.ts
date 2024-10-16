13c4ceaa209cd77057b2e1881d863e9719fb2a46
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-16 12:33:38 +0530

    len

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,9 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const hostname = req.headers.get("host") || "";
 
-  
   const subdomain = hostname.split(".")[0];
-  const hostLen = hostname.split(".").length < 2;
-  console.log("hn:", !hostLen);
+  const hostLen = hostname.split(".").length;
+  console.log("hn:", hostLen);
 
   if (subdomain !== "www" && !hostLen) {
     url.pathname = `/${subdomain}${url.pathname}`;
