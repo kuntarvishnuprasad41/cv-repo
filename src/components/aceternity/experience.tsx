6f380dbe763d21f7822a5b17dd7233d8530fcda7
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-19 18:20:37 +0530

    Print friendly

--- src/components/aceternity/experience.tsx
+++ src/components/aceternity/experience.tsx
@@ -61,7 +61,7 @@ export function Experience({
 
       <div
         className={cn(
-          "relative flex h-full w-full items-center justify-center border border-slate-800 bg-slate-900/[0.8] text-sm text-white antialiased backdrop-blur-xl",
+          "relative flex h-full w-full items-center justify-center border border-slate-800 bg-slate-900/[0.8] text-sm text-white antialiased backdrop-blur-xl print:border-none",
           className,
         )}
         style={{
