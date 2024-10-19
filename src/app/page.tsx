c6d6be58758b7b583357f005e43ae9e56f6d5659
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-19 18:06:25 +0530

    layout shift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className=" container relative mx-auto scroll-my-12 overflow-auto p-4 md:p-16 print:p-12">
-      <div className="mx-auto mt-1 flex w-full  max-w-3xl justify-between space-y-8  rounded-md   px-8 py-4 print:space-y-4">
+      <div className="mx-auto mt-1 flex w-full  max-w-3xl justify-between space-y-2  rounded-md   px-8 py-4 print:space-y-4">
         <div className="flex-1 space-y-3.5">
           <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
           <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground print:text-[12px]">
@@ -90,7 +90,7 @@ export default function Page() {
           <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
         </Avatar>
       </div>
-      <section className="mx-auto mt-8 w-full max-w-3xl  space-y-8   rounded-md bg-[#001534] px-8 py-4 print:space-y-4">
+      <section className="mx-auto -mt-1 w-full max-w-3xl  space-y-8   rounded-md bg-[#001534] px-8 py-4 print:space-y-4">
         {/* <HomeNav /> */}
 
         <Section>
