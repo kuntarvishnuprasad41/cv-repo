319342fa512d5509011a82c99add322d2487cb7a
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-20 19:20:05 +0530

    Blog layput

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+import React from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export const metadata: Metadata = {
+  title: "Vishnu Prasad K | Full Stack Developer",
+  description: "Next.js, typescript",
+};
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <head>
+        <title>Blog | Vishnu Prasad Kuntar</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </head>
+      <body>
+        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
+          {children}
+          <GoogleAnalytics gaId="G-0ENBD8K0JC" />
+        </ThemeProvider>
+      </body>
+      <Analytics />
+      <SpeedInsights />
+    </html>
+  );
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-black px-8">
+      <div>asd</div>
+      <div className="mx-auto   h-screen w-full max-w-3xl  space-y-8   rounded-md bg-[#001534] px-8 py-4 print:space-y-2">
+        page
+      </div>
+    </div>
+  );
 }
 
 export default page;
