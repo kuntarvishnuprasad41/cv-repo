acc2e60150d8b2632dbde0dfdb2e425e76221253
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-20 20:57:36 +0530

    mw for styling

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,13 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const hostname = req.headers.get("host") || "";
 
+  // Don't rewrite static files like CSS, JS, and images
+  if (url.pathname.startsWith("/_next") || url.pathname.startsWith("/static")) {
+    return NextResponse.next();
+  }
+
   const subdomain = hostname.split(".")[0];
   const hostLen = hostname.split(".").length;
- 
-  console.log(hostname.split(".")[1]);
 
   if (
     subdomain !== "www" &&
