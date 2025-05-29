3b09c7fa5c7e3362c33ba75e3ff1b4541bb48750
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-05-29 14:43:48 +0530

    build error fix

--- src/components/navbar/Nabar.tsx
+++ src/components/navbar/Nabar.tsx
@@ -73,7 +73,7 @@ export function HomeNav() {
                     <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                       <li className="row-span-3">
                         <NavigationMenuLink asChild>
-                          <a
+                          <Link
                             className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                             href="/"
                           >
@@ -92,7 +92,7 @@ export function HomeNav() {
                             <p className="text-sm leading-tight text-muted-foreground">
                               {fact}
                             </p>
-                          </a>
+                          </Link>
                         </NavigationMenuLink>
                       </li>
                       {section.items.map((item) => (
