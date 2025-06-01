df0c852ebc00a29880abe42a5b4ff491e4d2f61f
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-06-01 19:54:40 +0530

    Sitemap updates

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -24,20 +24,20 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "monthly",
       priority: 0.5,
     },
+    // {
+    //   url: `${siteConfig.site_domain}/authors`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.5,
+    // },
     {
-      url: `${siteConfig.site_domain}/authors`,
+      url: `${siteConfig.site_domain}/posts/categories`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: `${siteConfig.site_domain}/categories`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
-    },
-    {
-      url: `${siteConfig.site_domain}/tags`,
+      url: `${siteConfig.site_domain}/posts/tags`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
